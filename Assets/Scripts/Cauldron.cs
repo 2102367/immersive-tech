7b35efdaa50fce06a12c0421afa0e501ec32dfab
duhrnise <146590344+duhrnise@users.noreply.github.com>
2025-03-16 13:22:23 +0000

    added cauldron interactibles

---
+++ Assets/Scripts/Cauldron.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour
+{
+    public GameStateManager gsm;
+    void Start()
+    {
+        gsm = GameObject.Find("GameStateManager").GetComponent<GameStateManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {   
+        if(other.tag == "Mushroom"){
+            gsm.mushrooms++;
+            Destroy(other.gameObject);
+        }  
+    }
+}
--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameStateManager : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
+    //antidote ingredients
+    public int mushrooms;
+    public int crystals;
+    public int flowers;
+
+    public TMP_Text ingredientsTxt;
     void Start()
     {
         GameObject.Find("ObjectiveText").GetComponent<TextMeshProUGUI>().SetText("Objective: Find the recipe scroll for the antidote.");
@@ -16,6 +21,7 @@ public class GameStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ingredientsTxt.SetText($"Mushrooms: {mushrooms}/5\nCrystals: {crystals}/5\nFlowers: {flowers}/5");
+
     }
 }
