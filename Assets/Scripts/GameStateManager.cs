80d7209a87c384b11d88a383eec576d255c7eb1d
duhrnise <146590344+duhrnise@users.noreply.github.com>
2025-03-16 14:00:47 +0000

    ceaning up models folder

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -11,9 +11,14 @@ public class GameStateManager : MonoBehaviour
     public int crystals;
     public int flowers;
 
+    public GameObject portalArea;
+
     public TMP_Text ingredientsTxt;
     void Start()
     {
+        mushrooms = 0;
+        crystals = 0;
+        flowers = 0;
         GameObject.Find("ObjectiveText").GetComponent<TextMeshProUGUI>().SetText("Objective: Find the recipe scroll for the antidote.");
 
     }
@@ -22,6 +27,10 @@ public class GameStateManager : MonoBehaviour
     void Update()
     {
         ingredientsTxt.SetText($"Mushrooms: {mushrooms}/5\nCrystals: {crystals}/5\nFlowers: {flowers}/5");
+
+        if(mushrooms >= 5 && crystals >= 5 && flowers >= 5){
+            portalArea.SetActive(true);
+        }
 
     }
 }
