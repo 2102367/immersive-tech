0696b8b84be5d21ba012240f3ee230ffb24d9494
Denise Mills <d.mills3@rgu.ac.uk>
2025-04-04 14:21:13 +0100

    added dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+
+    public string[] dialogueLines;
+    private int index = 0;
+
+    void Start()
+    {
+        if (dialogueLines.Length > 0)
+        {
+            dialogueText.text = dialogueLines[index];
+        }
+    }
+
+    public void NextDialogue()
+    {
+        index++;
+
+        if (index >= dialogueLines.Length)
+        {
+            index = 0; // Loop back to the start
+        }
+
+        dialogueText.text = dialogueLines[index];
+    }
+}
